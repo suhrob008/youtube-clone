c7a8d21cff72788c679074f4917d320b3bf0036f
Your Name <you@example.com>
2023-10-13 23:42:02 +0500

    .

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import CategoryPillis from "./components/CategoryPillis";
+import { categories, videos } from "./data/home";
+import PageHeader from "./layouts/PageHeader";
+import VideoGridItem from "./components/VideoGridItem";
+import Sidebar from "./layouts/SideBar";
+import { SidebarProvider } from "./contexts/SidebarContext";
+
+const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+
+  return (
+    <SidebarProvider>
+      <div className="max-h-screen flex flex-col">
+        <PageHeader />
+
+        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+          <Sidebar />
+
+          <div className="overflow-x-hidden px-8 pb-4">
+            <div className="sticky top-0 bg-white z-10 pb-4">
+              <CategoryPillis
+                categories={categories}
+                selectedCategory={selectedCategory}
+                onSelect={setSelectedCategory}
+              />
+            </div>
+
+            <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+              {videos.map((video) => (
+                <VideoGridItem key={video.id} {...video} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </SidebarProvider>
+  );
+};
+
+export default App;
